e3811cb798ffc6fa9781411752ef83b24acd6fe5
kamiy2743 <kamiy2743@gmail.com>
2026-06-25 18:16:30 +0900

    License追加

---
+++ frontend/Assets/Scripts/License/LicenseContentSetting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameKit.License;
+using UnityEngine;
+
+namespace DA.License
+{
+    [CreateAssetMenu(fileName = "LicenseContentSetting", menuName = "DesktopAgent/License/LicenseContentSetting")]
+    public sealed class LicenseContentSetting : ScriptableObject, ILicenseContentValueProvider
+    {
+        [SerializeField] GameKitLicenseContentSetting gameKitLicenseContentSetting;
+        [SerializeField] LicenseContent[] daLicenseContents;
+
+        IReadOnlyList<LicenseContentValue> ILicenseContentValueProvider.Get()
+        {
+            var licenseContents = new List<LicenseContentValue>();
+            licenseContents.AddRange(((ILicenseContentValueProvider)gameKitLicenseContentSetting).Get());
+            licenseContents.AddRange(daLicenseContents.Select(x => new LicenseContentValue(x.Name, x.Body)));
+            return licenseContents;
+        }
+        
+        [Serializable]
+        public sealed record LicenseContent
+        {
+            [SerializeField] string name;
+            [SerializeField][TextArea] string body;
+            
+            public string Name => name;
+            public string Body => body;
+        }
+    }
+}
---
+++ frontend/Assets/Scripts/ScriptableObject/ScriptableObjectLifetimeScope.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DA.License;
+using GameKit.DependencyInjection;
+using GameKit.DependencyInjection.Base;
+using GameKit.License;
+using UnityEngine;
+using VContainer;
+
+namespace DA.ScriptableObject
+{
+    public sealed class ScriptableObjectLifetimeScope : BaseMBLifetimeScopeRegistration 
+    {
+        [SerializeField] LicenseContentSetting licenseContentSetting;
+
+        public override Type GetParentType()
+        {
+            return typeof(RootLifetimeScope);
+        }
+        
+        public override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterInstance<ILicenseContentValueProvider>(licenseContentSetting);
+        }
+    }
+}
